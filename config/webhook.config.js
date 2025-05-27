896c66a29516c57342ade70aeadb3cce510b4eea
JirAndro <chuckaflores@gmail.com>
2025-05-27 07:40:17 +0000

    zzzz

--- config/webhook.config.js
+++
@@ -1,8 +0,0 @@
-// config/webhook.config.js
-module.exports = {
-    // Reemplaza esto con el ID de Webhook que te dio PayPal
-    // al configurar el webhook en tu dashboard de desarrollador.
-    // Ejemplo: "82C71174CJ172374H" (el que viste en tus logs)
-    id: "82C71174CJ172374H" 
-};
-
